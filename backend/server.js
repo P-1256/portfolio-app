52bf07f3ca9f1f6411b2e4b2057c2330fe558ccd
P-1256 <158600496+P-1256@users.noreply.github.com>
2026-05-16 16:02:53 +0530

    Restrict CORS to specific origin for security

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({origin:"https://portfolio-app-green-psi.vercel.app/"}));
 app.use(express.json());
 
 app.use("/api/projects", projectRoutes);
